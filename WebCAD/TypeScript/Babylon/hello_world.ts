2ccc37801abb6c8dd745bf371c7684f6bbfd87f0
ststeiger <quandary82@hailmail.net>
2019-02-27 09:41:21 +0100

    Added earcut.d.ts, largely fixed example2

--- WebCAD/TypeScript/Babylon/hello_world.ts
+++ WebCAD/TypeScript/Babylon/hello_world.ts
@@ -4,7 +4,46 @@
 // interface Screen { readonly orientation: ScreenOrientation;
 // interface Document {      readonly fullscreen: boolean;
 
-let a = 5;
+
+namespace Babylon.Examples.No1
+{
 
 
-// BABYLON.scene
+
+    var canvas: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById("renderCanvas"); // Get the canvas element
+    var engine = new BABYLON.Engine(canvas, true); // Generate the BABYLON 3D engine
+
+    /******* Add the create scene function ******/
+    var createScene = function () {
+
+        // Create the scene space
+        var scene = new BABYLON.Scene(engine);
+
+        // Add a camera to the scene and attach it to the canvas
+        var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 5), scene);
+        camera.attachControl(canvas, true);
+
+        // Add lights to the scene
+        var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+        var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+
+        // Add and manipulate meshes in the scene
+        var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
+
+        return scene;
+    };
+    /******* End of the create scene function ******/
+
+    var scene = createScene(); //Call the createScene function
+
+    // Register a render loop to repeatedly render the scene
+    engine.runRenderLoop(function () {
+        scene.render();
+    });
+
+    // Watch for browser/canvas resize events
+    window.addEventListener("resize", function () {
+        engine.resize();
+    });
+
+}
--- WebCAD/wwwroot/ts/Babylon/hello_world.js
+++ WebCAD/wwwroot/ts/Babylon/hello_world.js
@@ -1,1 +1,27 @@
-let a = 5;
+var Babylon;
+(function (Babylon) {
+    var Examples;
+    (function (Examples) {
+        var No1;
+        (function (No1) {
+            var canvas = document.getElementById("renderCanvas");
+            var engine = new BABYLON.Engine(canvas, true);
+            var createScene = function () {
+                var scene = new BABYLON.Scene(engine);
+                var camera = new BABYLON.ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, new BABYLON.Vector3(0, 0, 5), scene);
+                camera.attachControl(canvas, true);
+                var light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+                var light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 1, -1), scene);
+                var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2 }, scene);
+                return scene;
+            };
+            var scene = createScene();
+            engine.runRenderLoop(function () {
+                scene.render();
+            });
+            window.addEventListener("resize", function () {
+                engine.resize();
+            });
+        })(No1 = Examples.No1 || (Examples.No1 = {}));
+    })(Examples = Babylon.Examples || (Babylon.Examples = {}));
+})(Babylon || (Babylon = {}));
